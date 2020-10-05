2254cfe7871446cfcd02d92427851c4ea3dfa694
Jean-Christophe Benoît <jc_ben@live.ca>
2020-10-05 16:59:54 -0400

    Connected three leds and they all react to the analog input

--- analogInputGIT.ino
+++ analogInputGIT.ino
@@ -1,4 +1,8 @@
-int sensorPin = A0;
+const int sensorPin = A0;
+const int greenLedPin = 10;
+const int blueLedPin = 9;
+const int redLedPin = 11;
+int outputValue = 0;
 int sensorValue = 0;
 
 void setup() {
@@ -11,6 +15,15 @@ void loop() {
   sensorValue = analogRead(sensorPin);
   Serial.println(sensorValue);
   //Serial.print(" ");
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
 
-  delay(500);
+  
+  analogWrite(greenLedPin, outputValue);
+  analogWrite(blueLedPin, outputValue);
+  analogWrite(redLedPin, outputValue);
+  
+  
+  //analogWrite(LEDPin, outputValue);
+
+  delay(2);
 }
