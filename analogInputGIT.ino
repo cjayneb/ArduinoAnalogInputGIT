70965d28cd5bdcd533c11025500ca1a8d8e83c61
Jean-Christophe Benoît <jc_ben@live.ca>
2020-10-15 14:01:31 -0400

    made every cleaner, added codde so that the leds flash once the potentiometer is turned all the way and made sure it worked

--- analogInputGIT.ino
+++ analogInputGIT.ino
@@ -1,21 +1,21 @@
 const int potentiometer_pin = A0;
-const int greenLedPin2 = 10;
-const int greenLedPin1 = 11;
-const int redLedPin = 9;
+const int green_led_pin2 = 10;
+const int green_led_pin1 = 11;
+const int red_led_pin = 9;
 int output_value = 0;
 int potentiometer_value = 0;
-const int firstTemp = 50;
-const int secondTemp = 150;
+const int first_temp = 50;
+const int second_temp = 150;
 
 void setup() {
   // put your setup code here, to run once:
   
-  pinMode(greenLedPin1, OUTPUT);
-  pinMode(greenLedPin2, OUTPUT);
-  pinMode(redLedPin, OUTPUT);
-  pinMode(greenLedPin1, LOW);
-  pinMode(greenLedPin2, LOW);
-  pinMode(redLedPin, LOW);
+  pinMode(green_led_pin1, OUTPUT);
+  pinMode(green_led_pin2, OUTPUT);
+  pinMode(red_led_pin, OUTPUT);
+  pinMode(green_led_pin1, LOW);
+  pinMode(green_led_pin2, LOW);
+  pinMode(red_led_pin, LOW);
 
   Serial.begin(9600);
 }
@@ -26,29 +26,38 @@ void loop() {
   Serial.println(potentiometer_value);
   output_value = map(potentiometer_value, 0, 1023, 0, 255);
 
-  if(output_value > 0 && output_value <= 50){
+  if(output_value >= 0 && output_value <= 50){
     
-    analogWrite(greenLedPin1, output_value);
-    digitalWrite(greenLedPin2, LOW);
-    digitalWrite(redLedPin, LOW);
+    analogWrite(green_led_pin1, output_value);
+    digitalWrite(green_led_pin2, LOW);
+    digitalWrite(red_led_pin, LOW);
     
   }
   else if(output_value > 50 && output_value <= 150){
-    analogWrite(greenLedPin1, firstTemp);
-    analogWrite(greenLedPin2, output_value);
-    digitalWrite(redLedPin, LOW);
+    analogWrite(green_led_pin1, first_temp);
+    analogWrite(green_led_pin2, output_value);
+    digitalWrite(red_led_pin, LOW);
   }
   else if(output_value > 150 && output_value <= 254){
-    analogWrite(greenLedPin1, firstTemp);
-    analogWrite(greenLedPin2, secondTemp);
-    analogWrite(redLedPin, output_value);
+    analogWrite(green_led_pin1, first_temp);
+    analogWrite(green_led_pin2, second_temp);
+    analogWrite(red_led_pin, output_value);
   }
   else{
-    analogWrite(greenLedPin1, output_value);
-    analogWrite(greenLedPin2, output_value);
-    analogWrite(redLedPin, output_value);
+    digitalWrite(red_led_pin, LOW);
+    digitalWrite(green_led_pin1, LOW);
+    digitalWrite(green_led_pin2, LOW);
+  
+    delay(500);
+
+    analogWrite(green_led_pin1, output_value);
+    analogWrite(green_led_pin2, output_value);
+    analogWrite(red_led_pin, output_value);
+
+    delay(500);
+    
   }
   
-
-  delay(1);
+  delay(2);
+  
 }
